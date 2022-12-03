31f05fca00ca4948e8c489d94bdb385da9e7f081
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2022-12-03 23:41:30 +0500

    changes

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from "@mui/icons-material/Delete";
+const List = ({ id, text, dltitems }) => {
+  return (
+    <>
+      <div className="items">
+        <li>{text}</li>
 
-const List = () => {
-  return <div>List</div>;
+        <Tooltip title="Delete">
+          <Button className="dltbtn" onClick={() => dltitems(id)}>
+            <DeleteIcon />
+          </Button>
+        </Tooltip>
+      </div>
+    </>
+  );
 };
 
 export default List;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,28 +1,120 @@
+// import React, { useState } from "react";
+// import "./style.css";
+// import Button from "@mui/material/Button";
+// import Tooltip from "@mui/material/Tooltip";
+// import AddIcon from "@mui/icons-material/Add";
+// import List from "./List";
+// const Todo = () => {
+//   const [todo, setTodo] = useState("");
+//   const [add, setAdd] = useState([]);
+//   const [validation, setValidation] = useState("");
+//   const setData = (e) => {
+//     // console.log(e.target.value);
+//     setTodo(e.target.value);
+//   };
+
+//   const additeams = () => {
+//     if (todo == "") {
+//       setValidation("Enter Your Todo");
+//     } else {
+//       setAdd(() => {
+//         return [...add, todo];
+//       });
+//       setTodo("");
+//       setValidation("");
+//     }
+//   };
+//   const dltitems = (id) => {
+//     const dltdata = add.filter((ele, ind) => {
+//       return ind !== id;
+//     });
+//     setAdd(dltdata);
+//   };
+//   return (
+//     <>
+//       <div className="main-container">
+//         <div className="todo-box">
+//           <img src="/book.png" alt="todoimg" className="todoimg" />
+//           <h2 className="heading">Write Your Todo List Here</h2>
+//           <h3 className="validation">{validation}</h3>
+//           <div className="input-container">
+//             <input
+//               type="text"
+//               name="todo"
+//               onChange={setData}
+//               value={todo}
+//               placeholder="Enter Your Task"
+//             />
+
+//             <Tooltip title="Add">
+//               <Button className="addbtn" onClick={additeams}>
+//                 <AddIcon />
+//               </Button>
+//             </Tooltip>
+//           </div>
+//           <div className="store-container">
+//             <ul>
+//               {add.map((ele, index) => {
+//                 return (
+//                   <>
+//                     <List id={index} text={ele} dltitems={dltitems} />
+//                   </>
+//                 );
+//               })}
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Todo;
 import React, { useState } from "react";
 import "./style.css";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import AddIcon from "@mui/icons-material/Add";
+import List from "./List";
+
 const Todo = () => {
   const [todo, setTodo] = useState("");
+
   const [add, setAdd] = useState([]);
-  console.log(add);
+
+  const [validation, setValidation] = useState("");
+
   const setData = (e) => {
     // console.log(e.target.value);
     setTodo(e.target.value);
   };
 
   const additeams = () => {
-    setAdd(() => {
-      return [...add, todo];
-    });
+    if (todo == "") {
+      setValidation("Enter Your Todo");
+    } else {
+      setAdd(() => {
+        return [...add, todo];
+      });
+      setTodo("");
+      setValidation("");
+    }
   };
+
+  const dltitems = (id) => {
+    const dltdata = add.filter((el, ind) => {
+      return ind !== id;
+    });
+    setAdd(dltdata);
+  };
+
   return (
     <>
       <div className="main-container">
         <div className="todo-box">
-          <img src="" alt="todoimg" />
+          <img src="/book.png" alt="todoimg" className="todoimg" />
           <h2 className="heading">Write Your Todo List Here</h2>
+          <h3 className="validation">{validation}</h3>
           <div className="input-container">
             <input
               type="text"
@@ -30,6 +122,7 @@ const Todo = () => {
               onChange={setData}
               value={todo}
               placeholder="Enter Your Task"
+              id=""
             />
 
             <Tooltip title="Add">
@@ -38,6 +131,18 @@ const Todo = () => {
               </Button>
             </Tooltip>
           </div>
+
+          <div className="store-container">
+            <ul>
+              {add.map((ele, index) => {
+                return (
+                  <>
+                    <List id={index} text={ele} dltitems={dltitems} />
+                  </>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </>
@@ -45,3 +150,6 @@ const Todo = () => {
 };
 
 export default Todo;
+
+// 1,2,3,4
+// 2
